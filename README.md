7bea37618a9450e3bef5b1e920b750aa457c0b99
KrzysztofMarciniak <96542207+KrzysztofMarciniak@users.noreply.github.com>
2025-06-21 23:51:49 +0200

    works

--- README.md
+++ README.md
@@ -3,11 +3,10 @@
 [doom emacs faq.org](https://github.com/doomemacs/doomemacs/blob/master/docs/faq.org#how-do-i-change-the-theme)
 
 ``` shell
-
 git clone https://github.com/KrzysztofMarciniak/doom-mono-dark-theme
 ```
-$DOOMDIR/config.el:
+`$DOOMDIR/config.el:`
 ``` emacs-lisp
 (add-to-list 'custom-theme-load-path "~/doom-mono-dark-theme")
-(setq doom-theme 'doom-mono-white-black)
+(setq doom-theme 'doom-mono-dark)
 ```
--- doom-mono-dark-theme.el
+++ doom-mono-dark-theme.el
@@ -1,103 +1,118 @@
 ;;; doom-mono-dark-theme.el -*- lexical-binding: t; no-byte-compile: t; -*-
+
 (require 'doom-themes)
 
+(defgroup doom-mono-dark-theme nil
+  "Options for the `doom-mono-dark' theme."
+  :group 'doom-themes)
+
+(defcustom doom-mono-dark-padded-modeline doom-themes-padded-modeline
+  "If non-nil, adds padding to the mode-line.
+Can be an integer for exact padding."
+  :group 'doom-mono-dark-theme
+  :type '(choice integer boolean))
+
 (def-doom-theme doom-mono-dark
-  "A minimalist dark Doom theme."
+  "A minimalist black & white Doom theme with red errors and yellow warnings."
 
-  ;; name        default   256       16
-  ((bg         '("#000000" nil       nil))
-   (bg-alt     '("#111111" nil       nil))
-   (base0      '("#FFFFFF" "white"   "white"))
-   (base1      '("#EEEEEE" "brightwhite" "brightwhite"))
-   (base2      '("#DDDDDD" "brightwhite" "brightwhite"))
-   (base3      '("#CCCCCC" "brightwhite" "brightwhite"))
-   (base4      '("#BBBBBB" "brightwhite" "brightwhite"))
-   (base5      '("#AAAAAA" "brightwhite" "brightwhite"))
-   (base6      '("#999999" "brightwhite" "brightwhite"))
-   (base7      '("#888888" "brightwhite" "brightwhite"))
-   (base8      '("#777777" "brightwhite" "brightwhite"))
-
+  ;; name       default    256       16
+  ((bg         '("#000000" "black"   "black"))
    (fg         '("#FFFFFF" "white"   "white"))
-   (fg-alt     '("#DDDDDD" "brightwhite" "brightwhite"))
 
-   (grey       base5)
-   (red        '("#FF5555" "red" "red"))
-   (orange     base0)
-   (green      base0)
-   (teal       base0)
-   (yellow     base0)
-   (blue       base0)
-   (dark-blue  base1)
-   (magenta    base0)
-   (violet     base0)
-   (cyan       base0)
-   (dark-cyan  base1)
+   (bg-alt     '("#111111" "black"   "black"))
+   (fg-alt     '("#AAAAAA" "grey"    "grey"))
 
-   ;; UI
-   (highlight      '("#333333" "brightblack" "brightblack"))
+   (base0      '("#000000" "black"   "black"))
+   (base1      '("#1A1A1A" "black"   "black"))
+   (base2      '("#333333" "black"   "black"))
+   (base3      '("#4D4D4D" "grey"    "grey"))
+   (base4      '("#666666" "grey"    "grey"))
+   (base5      '("#808080" "grey"    "grey"))
+   (base6      '("#999999" "grey"    "grey"))
+   (base7      '("#B3B3B3" "grey"    "grey"))
+   (base8      '("#CCCCCC" "grey"    "grey"))
+
+   (grey       base4)
+   (red        '("#FF0000" "red"     "red")) 
+   (orange     '("#FFAA00" "brightred" "yellow"))
+   (yellow     '("#FFFF00" "yellow"  "yellow")) 
+   (green      '("#FFFFFF" "white"   "white"))   
+   (teal       '("#FFFFFF" "white"   "white"))   
+   (blue       '("#FFFFFF" "white"   "white"))  
+   (dark-blue  '("#FFFFFF" "white"   "white"))  
+   (magenta    '("#FFFFFF" "white"   "white"))  
+   (violet     '("#FFFFFF" "white"   "white")) 
+   (cyan       '("#FFFFFF" "white"   "white"))   
+   (dark-cyan  '("#FFFFFF" "white"   "white"))   
+
+   (highlight      fg)
    (vertical-bar   base2)
-   (selection      '("#444444" "gray" "gray"))
+   (selection      base4)
    (builtin        fg)
-   (comments       base6)
+   (comments       base5)
    (doc-comments   base6)
    (constants      fg)
    (functions      fg)
-   (keywords       base1)
+   (keywords       fg)
    (methods        fg)
    (operators      fg)
    (type           fg)
-   (strings        base3)
+   (strings        fg)
    (variables      fg)
    (numbers        fg)
-   (region         selection)
+   (region         base3)
    (error          red)
-   (warning        base4)
-   (success        base3)
-   (vc-modified    grey)
-   (vc-added       grey)
-   (vc-deleted     base2)
+   (warning        yellow)
+   (success        fg)
+   (vc-modified    orange)
+   (vc-added       fg)
+   (vc-deleted     red)
 
-   ;; Modeline
-   (-modeline-pad (when doom-mono-dark-padded-modeline
-                    (if (integerp doom-mono-dark-padded-modeline)
-                        doom-mono-dark-padded-modeline
-                      4)))
-   (modeline-fg     fg)
-   (modeline-fg-alt base5)
-   (modeline-bg     base7)
-   (modeline-bg-l   base6)
-   (modeline-bg-inactive   base5)
-   (modeline-bg-inactive-l base4))
+   (modeline-fg          fg)
+   (modeline-fg-alt      base6)
+   (modeline-bg          (doom-darken bg 0.3))
+   (modeline-bg-alt      (doom-darken bg 0.4))
+   (modeline-bg-inactive base2)
+   (modeline-bg-inactive-alt base3)
 
-  ;; --- Faces ---
-  (((font-lock-comment-face &override) :slant 'italic)
-   ((line-number &override) :foreground base6 :background bg)
-   ((line-number-current-line &override) :foreground fg :background bg-alt)
+   (-modeline-pad
+    (when doom-mono-dark-padded-modeline
+      (if (integerp doom-mono-dark-padded-modeline)
+          doom-mono-dark-padded-modeline
+        4))))
+
+  (((line-number &override) :foreground base4)
+   ((line-number-current-line &override) :foreground fg)
+   ((font-lock-comment-face &override) :foreground base5)
    (mode-line
     :background modeline-bg :foreground modeline-fg
     :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg)))
    (mode-line-inactive
     :background modeline-bg-inactive :foreground modeline-fg-alt
     :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg-inactive)))
-   (mode-line-emphasis :foreground fg :weight 'bold)
+   (mode-line-emphasis :foreground fg)
 
-   (region :background selection)
+   (region :background base4)
+   (highlight :foreground fg)
+   (error :foreground red :weight 'bold)
+   (warning :foreground yellow :weight 'bold)
+   (success :foreground fg :weight 'bold)
+   (fringe :background bg)
+   (vertical-border :foreground base2)
    (cursor :background fg)
-   (fringe :background bg-alt)
-   (vertical-border :foreground vertical-bar)
-
-   ;; Highlighting
-   (highlight :background highlight)
-   (hl-line :background highlight)
-   (lazy-highlight :background highlight :weight 'bold)
-
-   ;; Syntax
-   (font-lock-builtin-face :foreground fg)
-   (font-lock-comment-face :foreground comments)
+   (font-lock-comment-face :foreground base5 :slant 'italic)
+   (font-lock-doc-face :foreground base6 :slant 'italic)
+   (font-lock-string-face :foreground fg)
+   (font-lock-keyword-face :foreground fg :weight 'bold)
    (font-lock-constant-face :foreground fg)
-   (font-lock-function-name-face :foreground fg)
-   (font-lock-keyword-face :foreground keywords :weight 'bold)
-   (font-lock-string-face :foreground strings)
-   (font-lock-type-face :foreground fg)
+   (font-lock-function-name-face :foreground fg :weight 'bold)
    (font-lock-variable-name-face :foreground fg)
-   (font-lock-warning-face :foreground red :weight 'bold)))
+   (font-lock-builtin-face :foreground fg)
+   (font-lock-type-face :foreground fg)
+   (isearch :background base3 :foreground fg :weight 'bold)
+   (lazy-highlight :background base2 :foreground fg)
+   (doom-modeline-bar :background fg)
+   (doom-modeline-buffer-file :inherit 'mode-line-buffer-id :weight 'bold)
+   (doom-modeline-buffer-path :inherit 'mode-line-emphasis :weight 'bold)
+   (doom-modeline-buffer-project-root :foreground fg :weight 'bold)))
+;;; doom-mono-dark-theme.el ends here
